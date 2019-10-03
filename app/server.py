00fca4b92e498265c66e7fd4106939c6cd4d35e0
Bruno Thienel <bruno.thienel@googlemail.com>
2019-10-03 19:32:10 +0200

    showing search suggestions

--- app/server.py
+++ app/server.py
@@ -18,13 +18,17 @@ async def ws_search(ws):
     while True:
         search_string = await ws.receive_text()
 
-        search_result = es.search(index=settings.ELASTICSEARCH_INDEX, q=search_string)
+        if not search_string:
+            await ws.send_json([])
+            continue
 
+        # TODO: could block the main loop.
         # TODO: exclude unwanted fields
+        search_result = es.search(index=settings.ELASTICSEARCH_INDEX, size=10, q=f'*"{search_string}"*')
 
-        await ws.send_json({})
+        await ws.send_json([hit['_source']['symbol'] for hit in search_result['hits']['hits']])
 
-    await ws.close()
+    # await ws.close()
 
 
 if __name__ == '__main__':
--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,6 +1,14 @@
 const ws = new WebSocket('ws://localhost:5042/ws/search');
+ws.onmessage = function(e) {show_suggestions(JSON.parse(e.data))};
+let autocomplete_instance = null;
 
-ws.onmessage = show_suggestions;
+document.addEventListener('DOMContentLoaded', function() {
+    let elements = document.querySelectorAll('.autocomplete');
+    M.Autocomplete.init(elements);
+
+    autocomplete_instance = M.Autocomplete.getInstance(document.getElementById('search'));
+    autocomplete_instance.onAutocomplete = function(e) {console.log(e)};
+});
 
 function search_suggestion(search_string) {
     ws.send(search_string);
@@ -9,7 +17,11 @@ function search_suggestion(search_string) {
 
 function show_suggestions(data) {
     // TODO: show suggestions below input field
+    let result = {};
     for (let i=0; i<data.length; i++) {
-
+        result[data[i]] = null;
     }
+
+    autocomplete_instance.updateData(result);
+    autocomplete_instance.open();
 }
